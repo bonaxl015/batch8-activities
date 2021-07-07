c7bb4494798abb1337ce05cbfab8ff8da5e4db5e
Bon Axl Feeser <bonaxl015@gmail.com>
2021-07-08 01:35:31 +0800

    Updated output printing of result

--- Ruby/exercises/3.2_unique_in_order.rb
+++ Ruby/exercises/3.2_unique_in_order.rb
@@ -2,6 +2,6 @@ def unique_in_order(input)
     input.class == String ? input.squeeze.split('') : input.class == Array ? input.join.squeeze.split('') : 'Invalid input'
 end
 
-puts unique_in_order('AAAABBBCCDAAABBB')
-puts unique_in_order('ABBCcAD')
-puts unique_in_order([1, 2, 2, 3, 3])
+p unique_in_order('AAAABBBCCDAAABBB')
+p unique_in_order('ABBCcAD')
+p unique_in_order([1, 2, 2, 3, 3])
